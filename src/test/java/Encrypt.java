161fa5c64d7b940702c31fee0fc58879192d5662
jordanh97 <47331061+jordanh97@users.noreply.github.com>
2019-02-13 17:35:01 +0200

    Encrypt Modified: Added basicEncrypt and moderateEncrypt. Main Method Created: basic cmd line input and output.

--- src/test/java/Encrypt.java
+++ src/test/java/Encrypt.java
@@ -1,3 +1,51 @@
+
+
+import java.util.ArrayList;
+
 public class Encrypt {
-    
+
+    private String input;
+
+    public String basicEncrypt(String i) {
+        input = i;
+        String output = "";
+
+        int k = input.length();
+        int h = 0;
+
+        do {
+            char charValue = input.charAt(h);
+            int newIntValue = (int) charValue + 3;
+            char newCharValue = (char)newIntValue;
+            output = new StringBuffer(output).insert(h, newCharValue).toString();
+
+            h++;
+
+        }while (h < k);
+
+        return output;
+    }
+
+    public String moderateEncrypt(String i) {
+        input = i;
+        String output = "";
+
+        int k = input.length();
+        int h = 0;
+
+        do {
+            char charValue = input.charAt(h);
+            int newIntValue = (int) charValue * 43;
+            int middleInt = newIntValue / 12;
+            int lastInt = middleInt -234;
+            char newCharValue = (char)lastInt;
+            output = new StringBuffer(output).insert(h, newCharValue).toString();
+
+            h++;
+
+        }while (h < k);
+
+        return output;
+    }
 }
+
