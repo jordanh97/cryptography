423972ff879c56134d57737d39f7419c3afd93b7
Saaligha <47171518+Saaligha@users.noreply.github.com>
2019-02-13 19:19:26 +0200

    I've made some changes concerning the dialog of this app

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,24 +4,24 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("----------------------------------------------------------------------------");
         System.out.println(" ");
-        System.out.print("PLEASE ENTER A STRING TO ENCRYPT: ");
+        System.out.print("PLEASE ENTER A MESSAGE TO BE ENCRYPTED: ");
 
         Scanner s = new Scanner(System.in);
         String userInput = s.nextLine();
 
         System.out.println(" ");
-        System.out.println("'?-/|?' ENCRYPTING '?-/|?'");
+        System.out.println("Please wait while your message is being encoded...");
         System.out.println(" ");
-        System.out.print("STRING AFTER ENCRYPTION: ");
+        System.out.print("YOUR MESSAGE AFTER ENCRYPTION: ");
 
         Encrypt encrypt = new Encrypt();
         System.out.print(encrypt.moderateEncrypt(userInput)); // change to encrypt.basicEncrypt for basic encryption
 
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println("----------------------------------------------------------------------------");
     }
 
 }
